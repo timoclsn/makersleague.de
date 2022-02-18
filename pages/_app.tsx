ea6211a91b14f554442435aee660405a9d3f0f18
Timo Clasen <goebeltimo@gmail.com>
2022-02-18 22:13:02 +0100

    Add Space Grotesk Font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css';
+import '@fontsource/space-grotesk/variable.css';
+
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Space GroteskVariable', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
