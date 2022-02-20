9862472e0b80644385a2b6d6da33b7e96a5de64b
Timo Clasen <goebeltimo@gmail.com>
2022-02-20 22:13:01 +0100

    Update HTML Order for Better Accessibility

--- components/Page.tsx
+++ components/Page.tsx
@@ -11,9 +11,15 @@ interface Props {
 export function Page({ children, expandedNav }: Props) {
   return (
     <>
-      <div className="flex flex-1 flex-col-reverse lg:flex-row-reverse">
+      <div className="flex flex-1 flex-col lg:flex-row">
         <Navigation expanded={expandedNav} />
-        <main className="bg-yellow-900 w-full">{children}</main>
+        <main
+          className={`bg-yellow-900 ${
+            expandedNav ? 'order-first' : 'order-last'
+          } flex w-full flex-1 flex-col lg:order-first`}
+        >
+          {children}
+        </main>
       </div>
       <Footer />
     </>
