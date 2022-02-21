851a4c254a0d4217ce796c6040c7fbe708b9eca7
Timo Clasen <goebeltimo@gmail.com>
2022-02-21 11:00:34 +0100

    Fix Footer Padding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="bg-dark px-10 py-8 text-light">
+    <footer className="bg-dark px-4 py-4 text-light md:px-10 md:py-8">
       <ul className="flex flex-col space-x-0 space-y-6 sm:flex-row sm:space-x-6 sm:space-y-0">
         <li>Impressum</li>
         <li>Datenschutzerklärung</li>
