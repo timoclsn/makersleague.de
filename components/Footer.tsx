28d8cb855445675ebb226defa9d41250f9e9525d
Timo Clasen <goebeltimo@gmail.com>
2022-02-21 20:04:02 +0100

    Update Footer Alignment

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
-    <footer className="bg-dark px-4 py-4 text-light md:px-10 md:py-8">
-      <ul className="flex flex-col space-x-0 space-y-6 sm:flex-row sm:space-x-6 sm:space-y-0">
+    <footer className="bg-dark px-4 py-8 text-light md:px-10 md:py-8">
+      <ul className="flex flex-col items-center space-x-0 space-y-6 sm:flex-row sm:space-x-6 sm:space-y-0">
         <li>Impressum</li>
         <li>Datenschutzerklärung</li>
         <li>Satzung</li>
