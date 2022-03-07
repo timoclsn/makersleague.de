4a1b1aa241aac26aab1bdd8e84a7c36be256ca38
Timo Clasen <goebeltimo@gmail.com>
2022-03-07 20:23:24 +0100

    Add Redirects
    Closes #13

--- next.config.js
+++ next.config.js
@@ -31,6 +31,50 @@ const nextConfig = {
 
     return config;
   },
+  async redirects() {
+    return [
+      {
+        source: '/haeufige-fragen',
+        destination: '/faqs',
+        permanent: true,
+      },
+      {
+        source: '/unsere-mitglieder',
+        destination: '/mitglieder',
+        permanent: true,
+      },
+      {
+        source: '/core-values',
+        destination: '/ueber#values',
+        permanent: true,
+      },
+      {
+        source: '/2021/04/30/1-aller-anfang-ist-leer',
+        destination: '/blog/aller-anfang-ist-leer',
+        permanent: true,
+      },
+      {
+        source: '/2021/05/18/2-vom-trauen-und-trauern',
+        destination: '/blog/vom-trauen-und-trauern',
+        permanent: true,
+      },
+      {
+        source: '/2021/06/07/3-aufbruchsstimmung/',
+        destination: '/blog/aufbruchsstimmung',
+        permanent: true,
+      },
+      {
+        source: '/2021/07/29/4-home-sweet-home',
+        destination: '/blog/home-sweet-home',
+        permanent: true,
+      },
+      {
+        source: '/2021/12/23/5-noch-einmal-mit-gefuehl/',
+        destination: '/blog/noch-einmal-mit-gefuehl',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withContentlayer()(nextConfig);
