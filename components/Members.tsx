c3269c0778759b8a63fae2d69469b5ba4b1da96d
Timo Clasen <goebeltimo@gmail.com>
2022-03-11 20:31:20 +0100

    Make Whole Member Item Clickable

--- components/Members.tsx
+++ components/Members.tsx
@@ -15,47 +15,55 @@ export function Members({ members }: Props) {
         <li
           className={`${
             idx === 0 ? 'oder-1' : 'order-last'
-          } flex w-[calc(50%-8px)] flex-col items-start border-4 p-4 md:w-[calc(33.33%-22px)]`}
+          } w-[calc(50%-8px)] border-4 p-4 md:w-[calc(33.33%-22px)]`}
           key={member.id}
         >
-          <div className="mb-1">
-            <Image
-              src={`/api/easyverein?url=${encodeURIComponent(
-                member.profilePicture
-              )}`}
-              alt={member.name}
-              width={700}
-              height={700}
-              quality={100}
-            />
-          </div>
-          <h3 className="break-word w-full text-base font-bold opacity-80 md:text-2xl">
-            {member.name}
-          </h3>
-          <p className="w-full break-words pb-14">{member.slogan}</p>
           <Link href={`/mitglieder/${member.slug}`}>
-            <a className="flex flex-1 items-center justify-center gap-1 self-end font-bold text-pink hover:opacity-80">
-              <Arrow className="text-2xl" />
-              mehr
+            <a className="group flex h-full flex-col items-start">
+              <div className="mb-1">
+                <Image
+                  src={`/api/easyverein?url=${encodeURIComponent(
+                    member.profilePicture
+                  )}`}
+                  alt={member.name}
+                  width={700}
+                  height={700}
+                  quality={100}
+                />
+              </div>
+              <h3 className="break-word w-full text-base font-bold opacity-80 md:text-2xl">
+                {member.name}
+              </h3>
+              <p className="w-full break-words pb-14">{member.slogan}</p>
+              <div className="flex w-full flex-1 flex-col items-end justify-end">
+                <span className="flex items-center justify-center gap-1 self-end font-bold text-pink group-hover:opacity-80">
+                  <Arrow className="text-2xl" />
+                  mehr
+                </span>
+              </div>
             </a>
           </Link>
         </li>
       ))}
-      <li className="order-2 flex w-[calc(50%-8px)] flex-col border-4 border-dashed p-4 text-pink md:w-[calc(33.33%-22px)]">
-        <div className="mb-1">
-          <Image
-            src="/assets/doodle-new-member-placeholder.svg"
-            alt="Mitmachen"
-            width={700}
-            height={700}
-          />
-        </div>
-        <h3 className="text-base font-bold opacity-80 md:text-2xl">Du?</h3>
-        <p className="pb-14">Macher*in mit Bock etwas zu bewegen!</p>
+      <li className="order-2 w-[calc(50%-8px)] border-4 border-dashed p-4 text-pink md:w-[calc(33.33%-22px)]">
         <Link href="/mitglied-werden">
-          <a className="flex flex-1 items-center justify-center gap-1 self-end font-bold text-pink hover:opacity-80">
-            <Arrow className="text-2xl" />
-            mehr
+          <a className="group flex h-full flex-col items-start">
+            <div className="mb-1">
+              <Image
+                src="/assets/doodle-new-member-placeholder.svg"
+                alt="Mitmachen"
+                width={700}
+                height={700}
+              />
+            </div>
+            <h3 className="text-base font-bold opacity-80 md:text-2xl">Du?</h3>
+            <p className="pb-14">Macher*in mit Bock etwas zu bewegen!</p>
+            <div className="flex w-full flex-1 flex-col items-end justify-end">
+              <span className="flex items-center justify-center gap-1 self-end font-bold text-pink group-hover:opacity-80">
+                <Arrow className="text-2xl" />
+                mehr
+              </span>
+            </div>
           </a>
         </Link>
       </li>
