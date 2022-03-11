528ef17207192d4750311845ff0cef4308494020
Timo Clasen <goebeltimo@gmail.com>
2022-03-11 14:03:38 +0100

    Add Hover States

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,28 +5,28 @@ export function Footer() {
   return (
     <footer className="bg-dark px-4 py-8 text-light md:px-10 md:py-8">
       <ul className="flex flex-col items-center space-x-0 space-y-6 sm:flex-row sm:space-x-6 sm:space-y-0">
-        <li>
+        <li className="hover:opacity-80">
           <a href="https://www.instagram.com/makersleague.ev">
             <span className="sr-only">Instagram</span>
             <Instagram className="text-2xl" />
           </a>
         </li>
-        <li>
+        <li className="hover:opacity-80">
           <Link href="/impressum">
             <a>Impressum</a>
           </Link>
         </li>
-        <li>
+        <li className="hover:opacity-80">
           <Link href="/datenschutzerklaerung">
             <a>Datenschutzerklärung</a>
           </Link>
         </li>
-        <li>
+        <li className="hover:opacity-80">
           <Link href="/docs/Makers_League_eV_Satzung.pdf">
             <a>Satzung</a>
           </Link>
         </li>
-        <li>
+        <li className="hover:opacity-80">
           <Link href="/docs/Makers_League_eV_Beitragsordnung.pdf">
             <a>Beitragsordnung</a>
           </Link>
