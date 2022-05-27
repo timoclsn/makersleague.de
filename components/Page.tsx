ea738d1cec1895d1672bf9b2ab9113afebe54fe3
Timo Clasen <goebeltimo@gmail.com>
2022-05-27 14:53:22 +0200

    Enable Selective Hydration

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 import Script from 'next/script';
 
 import { Footer } from './Footer';
@@ -33,37 +33,39 @@ export function Page({
       <Seo title={pageTitle} description={description} slug={slug} />
       <Favicons />
       <Script data-no-cookie data-api="/_hive" src="/bee.js" />
-      <div className="flex flex-1 flex-col xl:flex-row">
-        <Navigation expanded={expandedNav} />
-        <main
-          className={`bg-yellow-900 mx-auto flex w-full flex-1 flex-col p-4 md:p-10 xl:order-first ${
-            expandedNav ? 'order-first' : 'order-last'
-          }`}
-        >
-          <header className="mb-16 flex items-start justify-between md:mb-32">
-            <Link href="/">
-              <a className="hover:opacity-80">
-                <Logo className="text-[45px]" />
+      <Suspense fallback={null}>
+        <div className="flex flex-1 flex-col xl:flex-row">
+          <Navigation expanded={expandedNav} />
+          <main
+            className={`bg-yellow-900 mx-auto flex w-full flex-1 flex-col p-4 md:p-10 xl:order-first ${
+              expandedNav ? 'order-first' : 'order-last'
+            }`}
+          >
+            <header className="mb-16 flex items-start justify-between md:mb-32">
+              <Link href="/">
+                <a className="hover:opacity-80">
+                  <Logo className="text-[45px]" />
+                </a>
+              </Link>
+              <a
+                className="flex items-center justify-center space-x-2 hover:opacity-80"
+                href="https://easyverein.com/public/ML/"
+              >
+                <Profile />
+                <span>Mitgliederbereich</span>
               </a>
-            </Link>
-            <a
-              className="flex items-center justify-center space-x-2 hover:opacity-80"
-              href="https://easyverein.com/public/ML/"
-            >
-              <Profile />
-              <span>Mitgliederbereich</span>
-            </a>
-          </header>
-          {expandedNav ? (
-            children
-          ) : (
-            <div className="mx-auto flex w-full max-w-screen-xl flex-col gap-y-20 md:gap-y-32">
-              {children}
-            </div>
-          )}
-        </main>
-      </div>
-      <Footer />
+            </header>
+            {expandedNav ? (
+              children
+            ) : (
+              <div className="mx-auto flex w-full max-w-screen-xl flex-col gap-y-20 md:gap-y-32">
+                {children}
+              </div>
+            )}
+          </main>
+        </div>
+        <Footer />
+      </Suspense>
     </>
   );
 }
