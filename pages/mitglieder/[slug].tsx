8cd127a1ce38f7bd462a7e4bde217b8f831309d8
timoclsn <goebeltimo@gmail.com>
2023-03-13 16:35:27 +0100

    Fix ISR for New Member Pages

--- pages/mitglieder/[slug].tsx
+++ pages/mitglieder/[slug].tsx
@@ -108,7 +108,7 @@ export async function getStaticPaths() {
     paths: members.map((member) => ({
       params: { slug: member.slug },
     })),
-    fallback: false,
+    fallback: 'blocking',
   };
 }
 
