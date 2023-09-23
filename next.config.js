a68b7c6f864e13c34b5edf6cd66c62d780b807ac
timoclsn <goebeltimo@gmail.com>
2023-09-23 21:32:28 +0200

    Remove Middleware

--- next.config.js
+++ next.config.js
@@ -58,6 +58,18 @@ const nextConfig = {
 
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ];
+  },
   async redirects() {
     return [
       {
