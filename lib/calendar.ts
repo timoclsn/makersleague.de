19aecc64351dc4acaa3089696a7addc39d02d10f
timoclsn <goebeltimo@gmail.com>
2023-10-23 15:07:48 +0200

    Add Calendar Validation

--- lib/calendar.ts
+++ lib/calendar.ts
@@ -2,38 +2,49 @@ import { parseISO } from "date-fns";
 import ical from "ical";
 import { z } from "zod";
 
+// Todo: Filter for [WEBSITE] tag
+// Todo: Update [MEMBERS ONLY] tag handling
+
 const calendarUrl = z.string().parse(process.env.CALENDAR_URL);
+
+const eventSchema = z.object({
+  uid: z.string(),
+  type: z.string(),
+  summary: z.string().optional(),
+  description: z.string().optional(),
+  location: z.string().optional(),
+  start: z.string().optional(),
+});
 
 export const getEvents = async () => {
   const response = await fetch(calendarUrl);
   const body = await response.text();
   const rawCalendarData = ical.parseICS(body);
   const calenderDataArray = Object.values(rawCalendarData);
-  const now = new Date();
+  const parsedCalenderDataArray = z.array(eventSchema).parse(calenderDataArray);
+  const nowTime = new Date().getTime();
 
-  const events = calenderDataArray
+  const events = parsedCalenderDataArray
+    .filter(({ type, summary, start }) => {
+      if (!type.includes("VEVENT")) return false;
+      if (!summary) return false;
+      if (!start) return false;
+
+      const startTime = parseISO(start.replace("\r", "")).getTime();
+      if (startTime < nowTime) return false;
+
+      return true;
+    })
     .map((event) => {
       const startString = event.start as unknown as string;
       return {
         id: event.uid,
-        type: event.type,
         title: event.summary,
         description: event.description,
         location: event.location,
         start: parseISO(startString.replace("\r", "")),
         membersOnly: event?.description?.includes("MEMBERS ONLY."),
       };
-    })
-    .filter(({ type, title, start }) => {
-      if (!type.includes("VEVENT")) return false;
-      if (!title) return false;
-      if (!start) return false;
-
-      const nowTime = now.getTime();
-      const startTime = start.getTime();
-      if (startTime < nowTime) return false;
-
-      return true;
     })
     .sort((a, b) => a.start.getTime() - b.start.getTime());
 
