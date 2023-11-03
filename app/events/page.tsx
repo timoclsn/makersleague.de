8525c1e6987557557df9c06268efbd458c2b9569
Timo Clasen <goebeltimo@gmail.com>
2023-11-03 22:24:21 +0100

    Add Easyverein Calendar (#44)
    * Add Easyverein Calendar
    
    * Add Calendar Validation
    
    * Add tags for members only and website filtering in
    getEvents function
    
    * Add timezone support to calendar events
    
    * Update getEvents to use cached data.
    
    * Remove revalidate constant from events page
    
    * Refactor events page layout and add conditional
    rendering for events and no events case

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,69 +1,95 @@
-import { allEvents } from "contentlayer/generated";
-import { Arrow, Calendar, Location, Profile, Watch } from "icons";
+import { StructuredData } from "components/StructuredData";
+import { Calendar, Location, Profile } from "icons";
+import { getEventsCached } from "lib/calendar";
+import { formatDate } from "lib/utils";
 import { Metadata } from "next";
-import { Button } from "../../components/Button";
 
 export const metadata: Metadata = {
   title: "Events",
   description: "Makers League Events",
 };
 
-const EventsPage = () => {
-  const events = allEvents.sort((a, b) => {
-    const aInNumber = Number(new Date(a.startIso));
-
-    if (isNaN(aInNumber)) return -1; // Non formatted date comes first (e.g. Stammtisch)
-
-    const bInNumber = Number(new Date(b.startIso));
-    return aInNumber - bInNumber;
-  });
+const EventsPage = async () => {
+  const events = await getEventsCached();
 
   return (
-    <section>
-      <h1 className="mb-16 text-xl font-bold md:text-5xl">
-        Stammtisch & Events
-      </h1>
-      <div className="space-y-24">
-        {events.map((event) => (
-          <article key={event._id}>
-            <h2 className="mb-2 text-base font-bold md:text-2xl">
-              {event.title}
-            </h2>
-            <p className="mb-4 text-base opacity-60 md:text-2xl">
-              {event.subtitle}
-            </p>
-            <ul className="mb-8 flex flex-wrap gap-4 md:gap-8">
-              <li className="flex items-center justify-center gap-2 rounded-full bg-pink-light px-3 py-1 font-bold text-pink">
-                <Location className="text-xl" />
-                <span>{event.location}</span>
-              </li>
-              <li className="flex items-center justify-center gap-2 rounded-full bg-pink-light px-3 py-1 font-bold text-pink">
-                <Calendar className="text-xl" />
-                <span>{event.dateFormatted}</span>
-              </li>
-              <li className="flex items-center justify-center gap-2 rounded-full bg-pink-light px-3 py-1 font-bold text-pink">
-                <Watch className="text-xl" />
-                <span>{event.timeFormatted}</span>
-              </li>
-              <li className="flex items-center justify-center gap-2 rounded-full bg-pink-light px-3 py-1 font-bold text-pink">
-                <Profile className="text-xl" />
-                <span>{event.scale}</span>
-              </li>
-            </ul>
-            <div
-              className="prose mb-8"
-              dangerouslySetInnerHTML={{ __html: event.body.html }}
-            ></div>
-            {event.cta && (
-              <Button as="a" color="pink" href={event.cta.link}>
-                <Arrow className="text-2xl" />
-                {event.cta.text}
-              </Button>
-            )}
-          </article>
-        ))}
-      </div>
-    </section>
+    <>
+      {events.map((event) => (
+        <StructuredData key={event.id} type="Event">
+          {{
+            "@context": "https://schema.org",
+            "@type": "Event",
+            name: event.title,
+            startDate: event.start.toISOString(),
+            eventAttendanceMode:
+              "https://schema.org/OfflineEventAttendanceMode",
+            eventStatus: "https://schema.org/EventScheduled",
+            location: {
+              "@type": "Place",
+              name: "Makers Inn",
+              address: {
+                "@type": "PostalAddress",
+                streetAddress: "Küferstraße 46",
+                addressLocality: "Esslingen am Neckar",
+                postalCode: "73728",
+                addressRegion: "BW",
+                addressCountry: "DE",
+              },
+            },
+            description: event.description,
+            organizer: {
+              "@type": "Organization",
+              name: "Makers League e.V.",
+              url: "https://makersleague.de",
+            },
+          }}
+        </StructuredData>
+      ))}
+      <section>
+        <h1 className="mb-16 text-xl font-bold md:text-5xl">
+          Stammtisch & Events
+        </h1>
+        {events.length > 0 ? (
+          <div className="space-y-24">
+            {events.map((event) => (
+              <article key={event.id}>
+                <h2 className="mb-4 text-base font-bold md:text-2xl">
+                  {event.title}
+                </h2>
+                <ul className="mb-8 flex flex-wrap gap-4 md:gap-8">
+                  {event.location && (
+                    <li className="flex items-center justify-center gap-2 rounded-full bg-pink-light px-3 py-1 font-bold text-pink">
+                      <Location className="text-xl" />
+                      {event.location}
+                    </li>
+                  )}
+                  <li className="flex items-center justify-center gap-2 rounded-full bg-pink-light px-3 py-1 font-bold text-pink">
+                    <Calendar className="text-xl" />
+                    {`${formatDate(event.start, "dd. MMM yyyy | HH")} Uhr`}
+                  </li>
+                  <li className="flex items-center justify-center gap-2 rounded-full bg-pink-light px-3 py-1 font-bold text-pink">
+                    <Profile className="text-xl" />
+                    {event.membersOnly
+                      ? "Nur für Mitglieder"
+                      : "Jeder ist willkommen!"}
+                  </li>
+                </ul>
+                {event.description && (
+                  <div
+                    className="prose mb-8"
+                    dangerouslySetInnerHTML={{ __html: event.description }}
+                  />
+                )}
+              </article>
+            ))}
+          </div>
+        ) : (
+          <h2 className="mb-4 text-base font-bold md:text-2xl">
+            Keine anstehenden Events
+          </h2>
+        )}
+      </section>
+    </>
   );
 };
 
--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,10 +1,6 @@
-import {
-  defineDocumentType,
-  defineNestedType,
-  makeSource,
-} from "contentlayer/source-files";
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import { format, parseISO } from "date-fns";
 import readingTime from "reading-time";
-import { format, parseISO, parse, formatISO } from "date-fns";
 // @ts-ignore
 import { de } from "date-fns/locale/index.js"; // ESM import not working in node
 
@@ -56,79 +52,7 @@ const Faq = defineDocumentType(() => ({
   },
 }));
 
-const Cta = defineNestedType(() => ({
-  name: "CTA",
-  fields: {
-    text: { type: "string", required: true },
-    link: { type: "string", required: true },
-  },
-}));
-
-const Event = defineDocumentType(() => ({
-  name: "Event",
-  filePathPattern: "events/*.md",
-  contentType: "markdown",
-  fields: {
-    title: { type: "string", required: true },
-    subtitle: { type: "string", required: true },
-    location: { type: "string", required: true },
-    startTime: { type: "string", required: true },
-    endTime: { type: "string", required: true },
-    customDate: { type: "string", required: false },
-    scale: { type: "string", required: true },
-    cta: {
-      type: "nested",
-      of: Cta,
-    },
-  },
-  computedFields: {
-    dateFormatted: {
-      type: "string",
-      resolve: (doc) => {
-        if (doc.customDate) {
-          return doc.customDate;
-        }
-        return format(parse(doc.startTime, "Pp", new Date()), "dd. MMM yyyy", {
-          locale: de,
-        });
-      },
-    },
-    timeFormatted: {
-      type: "string",
-      resolve: (doc) => {
-        const timeFormat = doc.customDate ? "HH:mm" : "Pp";
-        const start = format(
-          parse(doc.startTime, timeFormat, new Date()),
-          "HH",
-        );
-        const end = format(parse(doc.endTime, timeFormat, new Date()), "HH");
-        return `${start} – ${end} Uhr`;
-      },
-    },
-    startIso: {
-      type: "string",
-      resolve: (doc) => {
-        try {
-          return formatISO(parse(doc.startTime, "Pp", new Date()));
-        } catch (e) {
-          return "";
-        }
-      },
-    },
-    endIso: {
-      type: "string",
-      resolve: (doc) => {
-        try {
-          return formatISO(parse(doc.endTime, "Pp", new Date()));
-        } catch (e) {
-          return "";
-        }
-      },
-    },
-  },
-}));
-
 export default makeSource({
   contentDirPath: "content",
-  documentTypes: [BlogPost, ContentPage, Faq, Event],
+  documentTypes: [BlogPost, ContentPage, Faq],
 });
