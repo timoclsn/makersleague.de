9d8fc61bd72ef1ac63feeb324cf5693c3cf6d196
timoclsn <goebeltimo@gmail.com>
2023-11-10 20:39:39 +0100

    Update Mitgliedsantrag page with styled section and responsive iframe

--- app/mitglied-werden/antrag/page.tsx
+++ app/mitglied-werden/antrag/page.tsx
@@ -1,9 +1,13 @@
 const ApplyPage = () => {
   return (
-    <iframe
-      src="https://easyverein.com/public/ML/applicationform/1099?iframe=True"
-      title="Vereinsverwaltungssoftware easyVerein"
-    />
+    <section className="border-4 p-4">
+      <h1 className="mb-6 text-xl font-bold md:text-5xl">Mitgliedsantrag</h1>
+      <iframe
+        src="https://easyverein.com/public/ML/applicationform/1099?iframe=True"
+        title="Vereinsverwaltungssoftware easyVerein"
+        style={{ width: "100%", height: "1500px", border: "none" }}
+      />
+    </section>
   );
 };
 
