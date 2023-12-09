bf1ba935d2da2f68456f1f21a9cd4ce795c68251
timoclsn <goebeltimo@gmail.com>
2023-12-09 20:17:38 +0100

    Update InstagramSection component to include links to Instagram posts

--- components/InstagramSection.tsx
+++ components/InstagramSection.tsx
@@ -2,13 +2,14 @@ import { getImagesCached } from "lib/instagram";
 import { Instagram } from "lucide-react";
 import Image from "next/image";
 import { Button } from "./Button";
+import Link from "next/link";
 
 export const InstagramSection = async () => {
   const images = await getImagesCached();
   return (
     <section id="instagram">
       <div className="mb-14">
-        <h2 className="mb-2 text-base font-bold md:text-2xl">Einblicke</h2>
+        <h2 className="mb-2 text-base font-bold md:text-2xl">Instagram</h2>
         <p className="mb-10 text-base opacity-60 md:text-2xl">
           Bilcke hinter die Kulissen der Makers League
         </p>
@@ -18,29 +19,40 @@ export const InstagramSection = async () => {
             const isVideo = image.media_url.includes("mp4");
             return (
               <li key={image.id} className="relative aspect-square border-4">
-                {isVideo ? (
-                  <video
-                    controls
-                    playsInline
-                    preload="metadata"
-                    src={image.media_url}
-                    className="absolute inset-0 h-full w-full object-cover p-4"
-                  />
-                ) : (
-                  <Image
-                    src={image.media_url}
-                    alt="Instagram Image"
-                    width={200}
-                    height={200}
-                    className="absolute inset-0 h-full w-full object-cover p-4"
-                  />
-                )}
+                <Link
+                  href={image.permalink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:opacity-80"
+                >
+                  {isVideo ? (
+                    <video
+                      controls
+                      playsInline
+                      preload="metadata"
+                      src={image.media_url}
+                      className="absolute inset-0 h-full w-full object-cover p-4"
+                    />
+                  ) : (
+                    <Image
+                      src={image.media_url}
+                      alt="Instagram Image"
+                      width={200}
+                      height={200}
+                      className="absolute inset-0 h-full w-full object-cover p-4"
+                    />
+                  )}
+                </Link>
               </li>
             );
           })}
         </ul>
       </div>
-      <Button href="https://www.instagram.com/makersleague.ev">
+      <Button
+        href="https://www.instagram.com/makersleague.ev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Instagram className="text-2xl" />
         Mehr auf Instagram
       </Button>
