e3093bca99d071e058552357688f398d846e5c59
timoclsn <goebeltimo@gmail.com>
2023-12-11 20:28:54 +0100

    Switch Hero Buttons

--- app/(home)/HeroButtons.tsx
+++ app/(home)/HeroButtons.tsx
@@ -9,6 +9,16 @@ import { usePathname } from "next/navigation";
 export const ApplyNowButton = () => {
   const pathname = usePathname();
 
+  const onMoreClick = () => {
+    const aboutUsSection = document.getElementById("about-us");
+    if (!aboutUsSection) return;
+    aboutUsSection.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+    track("Home hero learn more clicked");
+  };
+
   const onApplyNowClick = () => {
     const fitSection = document.getElementById("fit");
     if (!fitSection) return;
@@ -22,25 +32,15 @@ export const ApplyNowButton = () => {
     });
   };
 
-  const onMoreClick = () => {
-    const aboutUsSection = document.getElementById("about-us");
-    if (!aboutUsSection) return;
-    aboutUsSection.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-    track("Home hero learn more clicked");
-  };
-
   return (
     <div className="flex flex-col gap-4 sm:flex-row">
-      <Button variant="solid" color="dark" onClick={onApplyNowClick}>
-        <HeartPlus className="text-2xl" />
-        Jetzt bewerben
-      </Button>
-      <Button variant="outline" color="dark" onClick={onMoreClick}>
+      <Button variant="solid" color="dark" onClick={onMoreClick}>
         <ArrowDown />
         Mehr erfahren
+      </Button>
+      <Button variant="outline" color="dark" onClick={onApplyNowClick}>
+        <HeartPlus className="text-2xl" />
+        Jetzt bewerben
       </Button>
     </div>
   );
