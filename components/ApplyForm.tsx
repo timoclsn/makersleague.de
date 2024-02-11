6c4eddee2c901d02fbce1e6cda8a01a57f0b472c
timoclsn <goebeltimo@gmail.com>
2024-02-11 21:18:29 +0100

    Remove Padding

--- components/ApplyForm.tsx
+++ components/ApplyForm.tsx
@@ -1,6 +1,6 @@
 export const ApplyForm = () => {
   return (
-    <section className="border-4 p-4">
+    <section className="border-4">
       <iframe
         src="https://easyverein.com/public/ML/applicationform/1099?iframe=True"
         title="Vereinsverwaltungssoftware easyVerein"
