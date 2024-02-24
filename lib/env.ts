d2b519ba4130b1861f6a3fd8d8b2a593a796c024
Timo Clasen <goebeltimo@gmail.com>
2024-02-24 20:07:38 +0100

    Refactor Env Lib (#68)

--- lib/env.ts
+++ lib/env.ts
@@ -1,19 +1,33 @@
 import { z } from "zod";
 
+const envVarSchema = z.string().min(1);
+
 const envSchema = z.object({
-  NODE_ENV: z.enum(["development", "production"]),
-  PORT: z.string().min(1).optional(),
-  EASYVEREIN_TOKEN: z.string().min(1),
-  INSTAGRAM_USER_ID: z.string().min(1),
-  INSTAGRAM_ACCESS_TOKEN: z.string().min(1),
-  NEXT_PUBLIC_VERCEL_URL: z.string().min(1).optional(),
-  NEXT_PUBLIC_VERCEL_ENV: z.enum(["preview", "production"]).optional(),
+  // Server
+
+  // Node.js
+  PORT: envVarSchema.optional(),
+
+  // EasyVerein
+  EASYVEREIN_TOKEN: envVarSchema,
+
+  // Instagram
+  INSTAGRAM_USER_ID: envVarSchema,
+  INSTAGRAM_ACCESS_TOKEN: envVarSchema,
+
+  // Client
+
+  // Vercel
+  NEXT_PUBLIC_VERCEL_ENV: z
+    .enum(["production", "preview", "development"])
+    .optional(),
+  NEXT_PUBLIC_VERCEL_URL: envVarSchema.optional(),
 });
 
 envSchema.parse(process.env);
 
 declare global {
   namespace NodeJS {
-    interface ProcessEnv extends z.infer<typeof envSchema> {}
+    interface ProcessEnv extends z.input<typeof envSchema> {}
   }
 }
