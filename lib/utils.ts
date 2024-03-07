bf865906a36d1e7a52ae180eb489ca108eb05a5a
timoclsn <goebeltimo@gmail.com>
2024-03-07 22:21:21 +0100

    Update Image Optimization

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,8 +5,10 @@ const NEXT_PUBLIC_VERCEL_ENV = process.env.NEXT_PUBLIC_VERCEL_ENV;
 const NEXT_PUBLIC_VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
 const PORT = process.env.PORT;
 
+export const isProductionDeployment = NEXT_PUBLIC_VERCEL_ENV === "production";
+
 export const getBaseUrl = () => {
-  if (NEXT_PUBLIC_VERCEL_ENV === "production") return `https://makersleague.de`;
+  if (NEXT_PUBLIC_VERCEL_ENV === "production") return "https://makersleague.de";
   if (NEXT_PUBLIC_VERCEL_URL) return `https://${NEXT_PUBLIC_VERCEL_URL}`;
   return `http://localhost:${PORT ?? 3000}`;
 };
