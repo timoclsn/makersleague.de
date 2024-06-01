056d7b3ed301d6f5c4a0d950cba85e96319de647
timoclsn <goebeltimo@gmail.com>
2024-06-01 21:27:46 +0200

    Update HTML Sematics

--- components/TestimonialsSection.tsx
+++ components/TestimonialsSection.tsx
@@ -22,49 +22,58 @@ export const TestimonialsSection = async () => {
           const member = await getWebsiteMember(testimonial.data.name);
 
           return (
-            <li
-              key={index}
-              className="flex flex-1 flex-col items-center gap-4 bg-blue p-4 text-dark sm:p-8"
-            >
-              <div className="mb-4 flex w-full items-center gap-4">
-                <div className="relative flex-none">
-                  <div className="absolute -left-4 -top-4 flex items-center justify-center rounded-full bg-dark p-2 text-light">
-                    <Quote className="size-6 sm:size-8" />
+            <li key={index} className="flex-1">
+              <article className="h-full">
+                <figure className="flex h-full flex-col items-center gap-4 bg-blue p-4 text-dark sm:p-8">
+                  {/* Head */}
+                  <div className="mb-4 flex w-full items-center gap-4">
+                    {/* Image */}
+                    <div className="relative flex-none">
+                      <div className="absolute -left-4 -top-4 flex items-center justify-center rounded-full bg-dark p-2 text-light">
+                        <Quote className="size-6 sm:size-8" />
+                      </div>
+                      <MemberImage
+                        member={member}
+                        className="block size-24 rounded-full"
+                      />
+                    </div>
+
+                    {/* Name and slogan */}
+                    <figcaption>
+                      <h3 className="text-base font-bold md:text-2xl">
+                        {member.name}
+                      </h3>
+                      <p className="text-sm opacity-60 sm:text-base">
+                        {member.slogan}
+                      </p>
+                    </figcaption>
                   </div>
-                  <MemberImage
-                    member={member}
-                    className="block size-24 rounded-full"
-                  />
-                </div>
-                <div>
-                  <h3 className="text-base font-bold md:text-2xl">
-                    {member.name}
-                  </h3>
-                  <p className="text-sm opacity-60 sm:text-base">
-                    {member.slogan}
-                  </p>
-                </div>
-              </div>
 
-              <div className="prose text-sm">{testimonial.content}</div>
+                  {/* Content */}
+                  <blockquote className="prose mb-8 max-w-none text-sm">
+                    {testimonial.content}
+                  </blockquote>
 
-              <Track
-                event="Testimonial clicked"
-                data={{
-                  name: member.name,
-                }}
-                className="mt-8 flex w-full flex-col md:items-start"
-              >
-                <Button
-                  color="dark"
-                  variant="outline"
-                  size="small"
-                  href={`/mitglieder/${member.slug}`}
-                >
-                  <Arrow className="text-2xl" />
-                  Steckbrief
-                </Button>
-              </Track>
+                  {/* Button */}
+                  <Track
+                    event="Testimonial clicked"
+                    data={{
+                      name: member.name,
+                    }}
+                    className="flex w-full flex-col md:items-start"
+                  >
+                    <Button
+                      color="dark"
+                      variant="outline"
+                      size="small"
+                      href={`/mitglieder/${member.slug}`}
+                    >
+                      <Arrow className="text-2xl" />
+                      Steckbrief
+                    </Button>
+                  </Track>
+                </figure>
+              </article>
             </li>
           );
         })}
