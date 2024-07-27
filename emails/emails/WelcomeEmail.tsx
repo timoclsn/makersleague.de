b5550332a7d669a183dc06e72076715d4af6a2f0
timoclsn <goebeltimo@gmail.com>
2024-07-27 21:35:07 +0200

    Add E-Mail Content

--- emails/emails/WelcomeEmail.tsx
+++ emails/emails/WelcomeEmail.tsx
@@ -48,6 +48,7 @@ export const WelcomeEmail = ({ firstName }: Props) => {
               herzlich willkommen in der Makers League! Wir freuen uns sehr,
               dich als neues Mitglied unserer Community zu begrüßen.
             </Text>
+
             <Heading as="h2">❤️ Unsere Werte</Heading>
             <Text>
               Unser Vereinsleben basiert auf den drei Werten <Bold>O</Bold>
@@ -56,7 +57,6 @@ export const WelcomeEmail = ({ firstName }: Props) => {
               findest du eine ausführliche Beschreibung unserer Werte. Uns ist
               sehr wichtig, dass diese Werte wertgeschätzt und gelebt werden.
             </Text>
-
             <Text>
               Zudem sei gesagt: Wir sind ein junger Verein und bauen gemeinsam
               mit dir unsere Community auf. Noch ist nicht alles perfekt, aber
@@ -66,9 +66,102 @@ export const WelcomeEmail = ({ firstName }: Props) => {
               machen oder eigene Circles und Projekte ins Leben zu rufen – wir
               freuen uns über jedes Engagement und jede Idee.
             </Text>
-
             <Text>
               <Italic>Wo findest du die anderen Macher:innen?</Italic>
+            </Text>
+
+            <Heading as="h2">📱 Whatsapp Community</Heading>
+            <Text>
+              Trete mit{" "}
+              <Link href="https://chat.whatsapp.com/HHrRl5Gl2nwJlKO6BIhYax">
+                diesem Link
+              </Link>{" "}
+              unserer WhatsApp Community bei und bleibe immer auf dem Laufenden!
+              Im Makers League Chat kannst du den anderen Mitgliedern Fragen
+              stellen, Tipps teilen und sonstige News rund um unsere Community
+              erfahren. Zusätzlich kannst du gerne weiteren Gruppen in der
+              Makers League Community beitreten.
+            </Text>
+
+            <Heading as="h2">🍻 Stammtisch und Events</Heading>
+            <Text>
+              Unser Stammtische und Inspire Talks findet üblicherweise am 2. und
+              4. Dienstag jedes Monats um 18:00 Uhr im Makers Inn statt.
+              Zusätzlich bieten wir monatlich eine Makers League Masterclass an
+              und ab und an weitere Specials wie Workshops oder Meetups.
+              <Link href="https://makersleague.de/events">Hier</Link>
+              findest du alle Termine und kannst dich direkt für unser Community
+              Events anmelden.
+            </Text>
+
+            <Heading as="h2">☑️ Communitymanagement</Heading>
+            <Text>
+              Unsere Community wird über die Vereinssoftware Easyverein
+              organisiert. Den Einladungslink mit deinen Login-Daten für den
+              Zugang zu unserem Mitgliederbereich (Vereinskürzel ML) hast du
+              gerade per E-Mail erhalten. Hier kannst du deine persönlichen
+              Daten bearbeiten, findest die Kontakte der anderen Mitglieder und
+              kannst dich zu unseren Events anmelden. Wir empfehlen die
+              Installation der Easyverein App (verfügbar für{" "}
+              <Link href="https://apps.apple.com/de/app/easyverein-vereinsverwaltung/id1582761507">
+                iOS
+              </Link>{" "}
+              und{" "}
+              <Link href="https://play.google.com/store/apps/details?id=com.softwaredesign.easyverein&hl=de&pli=1">
+                Android
+              </Link>
+              ), alternativ kannst du dich aber auch über unsere Website im
+              <Link href="https://easyverein.com/public/ML/">
+                Mitgliederbereich
+              </Link>{" "}
+              einloggen.
+            </Text>
+
+            <Heading as="h2">🦸‍♂️🦸‍♀️ Unsere & deine Superkräfte</Heading>
+            <Text>
+              Jede:r von uns hat andere “Superkräfte”. So wollen wir uns
+              gemeinsam unterstützen und voneinander lernen. Unsere Mitglieder
+              erstellen daher kurze Steckbriefe mit ihren drei stärksten
+              Kompetenzen.{" "}
+              <Link href="https://makersleague.de/mitglieder">
+                Beispiel gefällig
+              </Link>
+              ? Bitte fülle deinen Steckbrief im Easyverein Mitgliederbereich
+              aus und lade ein Profilbild hoch. Du findest den entsprechenden
+              Reiter im Menü. Schicke zusätzlich ein möglichst hochauflösendes
+              Portraitbild (Betreff Steckbrief) per Mail an{" "}
+              <Link href="mailto:daniela@makersleague.de">Lela</Link>. Dadurch
+              wirst auch du bald Teil unserer Macher:innen Galerie im Makers Inn
+              und auf unserer Website.
+            </Text>
+
+            <Heading as="h2">🤨 Weitere Fragen</Heading>
+            <Text>
+              Du hast sicher noch einige Fragen zur Makers League.{" "}
+              <Link href="https://makersleague.de/haeufige-fragen/">Hier</Link>{" "}
+              beantworten wir dir die am häufigsten gestellten Fragen. Solltest
+              du weitere Fragen haben kannst du uns jederzeit eine E-Mail an{" "}
+              <Link href="mailto:hello@makersleague.de">
+                hello@makersleague.de
+              </Link>{" "}
+              senden.
+            </Text>
+
+            <Text>
+              Jetzt kann es für dich losgehen! Wir freuen uns auf dich!
+              <br />
+              <Bold>Till, Nina, Jochen, Piet und Norman</Bold>
+              <br />
+              Dein Makers League Vorstand
+            </Text>
+
+            <Text>
+              PS: Du bist auf Instagram oder LinkedIn aktiv? Dann vergiss nicht
+              uns zu folgen und uns für Reposts zu verlinken{" "}
+              <Link href="https://linktr.ee/makersleague">
+                Hier geht's zu unseren Kanälen
+              </Link>
+              .
             </Text>
           </Container>
         </Body>
