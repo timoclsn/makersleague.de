46252432339bbbd1895b4a21152ba480a807a5ba
timoclsn <goebeltimo@gmail.com>
2024-08-30 21:24:20 +0200

    feat: Add InfoBox component

---
+++ components/InfoBox.tsx
@@ -0,0 +1,27 @@
+import { cva, VariantProps } from "cva";
+import { Info } from "lucide-react";
+import { ReactNode } from "react";
+import { colorMap } from "./utils";
+
+const infoBox = cva({
+  base: "mt-6 flex items-baseline gap-2 p-6",
+  variants: {
+    color: {
+      blue: [colorMap.blue.bg, colorMap.blue.text],
+    },
+  },
+});
+
+interface Props extends VariantProps<typeof infoBox> {
+  children: ReactNode;
+  className?: string;
+}
+
+export const InfoBox = ({ children, color = "blue", className }: Props) => {
+  return (
+    <div className={infoBox({ color, className })}>
+      <Info size={16} className="flex-none" />
+      <p>{children}</p>
+    </div>
+  );
+};
