fca1ac9f342b74231b4f5a997ce53633b23fa325
timoclsn <goebeltimo@gmail.com>
2024-11-30 21:28:20 +0100

    Update gallery layout

--- app/makers-inn/Gallery.tsx
+++ app/makers-inn/Gallery.tsx
@@ -6,12 +6,15 @@ import { useState } from "react";
 import img1 from "../../public/assets/makers-inn/1.png";
 import img2 from "../../public/assets/makers-inn/2.png";
 import img3 from "../../public/assets/makers-inn/3.png";
+import img4 from "../../public/assets/makers-inn/1.png";
+import img5 from "../../public/assets/makers-inn/2.png";
+import img6 from "../../public/assets/makers-inn/3.png";
 
 const Lightbox = dynamic(() =>
   import("@/components/Lightbox").then((mod) => mod.Lightbox),
 );
 
-const images = [img1, img2, img3];
+const images = [img1, img2, img3, img4, img5, img6];
 
 export const Gallery = () => {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
@@ -19,10 +22,14 @@ export const Gallery = () => {
 
   return (
     <>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {images.map((image, index) => (
           <div key={index} className="relative">
-            <Image src={image} alt={`Makers Inn Bild ${index + 1}`} />
+            <Image
+              src={image}
+              alt={`Makers Inn Bild ${index + 1}`}
+              className="h-full object-cover"
+            />
             <button
               className="absolute bottom-0 left-0 right-0 top-0 h-full w-full"
               onClick={() => setOpenIndex(index)}
