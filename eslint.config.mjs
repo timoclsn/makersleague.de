8df447a926ae4e678dee7a72b38cd4e9b264147c
timoclsn <goebeltimo@gmail.com>
2024-12-25 20:19:18 +0100

    Use simpler eslint config after updating node js version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,7 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [...compat.extends("next/core-web-vitals", "prettier")];
