c3d3e9a46ec58b49f4bdbbb647cc05ceb6067f6b
timoclsn <goebeltimo@gmail.com>
2024-12-25 20:48:24 +0100

    Update board members in email footer

--- emails/components/Footer.tsx
+++ emails/components/Footer.tsx
@@ -24,7 +24,7 @@ export const Footer = ({ greeting }: Props) => {
         {greeting}
         <br />
         <br />
-        <Bold>Till, Nina, Jochen, Piet und Norman</Bold>
+        <Bold>Till, Nina, Patrick, Uta und Norman</Bold>
         <br />
         <Italic>Dein Makers League Vorstand</Italic>
       </Text>
