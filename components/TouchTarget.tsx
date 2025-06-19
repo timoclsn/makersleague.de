78b0e44ee3818b7c874d72c329b2932e7f3cc476
timoclsn <goebeltimo@gmail.com>
2025-06-19 21:31:30 +0200

    Update tailwind to version 4

--- components/TouchTarget.tsx
+++ components/TouchTarget.tsx
@@ -10,7 +10,7 @@ export const TouchTarget = ({ children }: Props) => {
     <>
       {children}
       <span
-        className="absolute left-1/2 top-1/2 size-[max(100%,2.75rem)] -translate-x-1/2 -translate-y-1/2 [@media(pointer:fine)]:hidden"
+        className="absolute left-1/2 top-1/2 size-[max(100%,2.75rem)] -translate-x-1/2 -translate-y-1/2 pointer-fine:hidden"
         aria-hidden="true"
       />
     </>
